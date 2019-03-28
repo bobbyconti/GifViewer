f713f5ba1ed421a767540bc79c7321109619432a
Bobby Conti <robert.conti@volunteer.hillsong.com>
2019-03-28 17:48:19 -0400

    Initial Commit

---
+++ GifViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GifViewer
+//
+//  Created by Bobby Conti on 3/28/19.
+//  Copyright © 2019 Bobby Conti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GifViewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GifViewer
+//
+//  Created by Bobby Conti on 3/28/19.
+//  Copyright © 2019 Bobby Conti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
