83af7495d997174b260847146c4d81dfd7731021
Bobby Conti <robert.conti@volunteer.hillsong.com>
2019-03-30 14:54:50 -0400

    Added tap and swipe gestures to dismiss

--- GifViewer/DetailViewController.m
+++ GifViewer/DetailViewController.m
@@ -23,6 +23,38 @@
         
         [self.webView loadRequest:request];
     }
+    
+    [self setupGestures];
+}
+
+- (void)setupGestures {
+    UITapGestureRecognizer *dismiss = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismiss)];
+    
+    dismiss.numberOfTapsRequired = 1;
+    [self.view addGestureRecognizer:dismiss];
+    
+    UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeToDismiss)];
+    
+    swipe.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:swipe];
+}
+
+- (void)dismiss {
+    
+    [UIView animateWithDuration:0.75 animations:^{
+        self.view.transform = CGAffineTransformMakeScale(0.01, 0.01);
+        self.view.alpha = 0.0;
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
+}
+
+- (void)swipeToDismiss {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view.transform = CGAffineTransformMakeTranslation(400, 0);
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
 }
 
 /*
